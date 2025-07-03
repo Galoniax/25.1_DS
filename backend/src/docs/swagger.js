e97dfa6fbae2e74db55d58cc5d79251444ad50cc
Galoniax <axelian2504@gmail.com>
2025-07-03 15:37:42 -0300

    Feat Test + Documentation

--- backend/src/docs/swagger.js
+++ backend/src/docs/swagger.js
@@ -1,11 +1,13 @@
 import swaggerUi from 'swagger-ui-express';
 
 import YAML from 'yamljs';
+import { DOCS_PREFIX } from '../config/config.js';
+
 
 const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
 
 export const swaggerDocs = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use(`${DOCS_PREFIX}`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
 
