5fd25c103c4cb35093bbe1e49147602417e8038e
Galoniax <axelian2504@gmail.com>
2025-07-03 15:37:01 -0300

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,2 +1,4 @@
 export const SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
-export const API_PREFIX = ["/api/v1", "/api-docs"];
+
+export const API_PREFIX = "/api/v1";
+export const DOCS_PREFIX = "/api-docs";
